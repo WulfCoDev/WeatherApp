8866b2e0010a487dfe98bf0d43cce6d909b4df74
WulfCoDev <126393550+WulfCoDev@users.noreply.github.com>
2024-04-08 21:31:49 -0700

    ts errors

--- src/CurrentTemp.tsx
+++ src/CurrentTemp.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { useWeather } from "./utils/RealTimeWeatherContext";
 
 const CurrentTemp = () => {
--- src/Home.tsx
+++ src/Home.tsx
@@ -1,7 +1,6 @@
 import CurrentTemp from "./CurrentTemp";
 import LocationInputForm from "./SearchBar";
-import { useWeather } from "./utils/RealTimeWeatherContext";
-import { useState } from "react";
+
 const Home = () => {
   return (
     <div className="bg-bg bg-cover bg-center bg-no-repeat flex flex-col h-screen">
