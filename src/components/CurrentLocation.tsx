b47d317cf8589fe765ef43ba74d1e527df7c5f50
WulfCoDev <wulfcodev@gmail.com>
2024-04-13 20:31:54 -0700

    bug

--- src/components/CurrentLocation.tsx
+++ src/components/CurrentLocation.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useWeather } from "../utils/RealTimeWeatherContext";
 
 const CurrentLocation = () => {
